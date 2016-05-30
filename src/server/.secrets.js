a72b24eadc77aada7d220d9016de8990c4995e0f
Caleb Jares <cable729@gmail.com>
2016-05-30 17:59:02 -0600

    Separate code into client and server directories

---
+++ src/server/.secrets.js
@@ -0,0 +1,2 @@
+export const TWITCH_API_KEY = '5a45tqlz3gkp7974yzt6jfzwfe6apqo';
+export const RIOT_API_KEY = '9c554018-a958-4bb7-b898-c85eda7978ff';
--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: [
     'webpack-dev-server/client?http://localhost:3000',
     'webpack/hot/only-dev-server',
-    './src/client'
+    './src/client/index'
   ],
   output: {
     path: path.join(__dirname, 'dist'),
